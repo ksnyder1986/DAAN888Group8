b9e12b2f6f9a7d99f7f0b38d212230f8c2623006
ksnyder1986 <54869316+ksnyder1986@users.noreply.github.com>
2019-10-30 07:14:41 -0400

    Update inputs.r

--- inputs.r
+++ inputs.r
@@ -3,11 +3,19 @@ library(dplyr)
 
 summary(new_reviews)
 
-user_list <- summaryBy(stars ~ user_id, data=new_reviews, FUN = c(length))
-user_list <- user_list[user_list$stars.length > 3,]
-new_reviews <- new_reviews %>% filter(user_id %in% user_list$user_id)
+user_list <- summaryBy(stars ~ user_id, data=new_reviews2, FUN = c(length))
+user_list <- user_list[user_list$stars.length > 2,]
+new_reviews <- new_reviews2 %>% filter(user_id %in% user_list$user_id)
 
-summaryby()
+
+user_revs <- summaryBy(service+speed+cleanliness+taste+
+                         environment+location+Eastern_Asian+Japanese+
+                         Mexican+Indian+Southeast_Asian+American+Italian+
+                         Middle_Eastern+Latin_American+Mediterranean+
+                         European+Fastfood+Seafood+Pizza+Breakfast+
+                         Cafe+Diners+Bars+other
+                       ~ user_id, data=new_reviews, FUN=c(mean,length))
+
 
 new_reviews$service_rating <- new_reviews$service*(new_reviews$stars+new_reviews$ave_sentiment)
 new_reviews$speed_rating <- new_reviews$speed*(new_reviews$stars+new_reviews$ave_sentiment)
@@ -16,92 +24,48 @@ new_reviews$taste_rating <- new_reviews$taste*(new_reviews$stars+new_reviews$ave
 new_reviews$environment_rating <- new_reviews$environment*(new_reviews$stars+new_reviews$ave_sentiment)
 new_reviews$location_rating <- new_reviews$location*(new_reviews$stars+new_reviews$ave_sentiment)
 
+
+bus_revs <- summaryBy(service_rating+speed_rating+cleanliness_rating+taste_rating+environment_rating+location_rating ~ business_id, data=new_reviews, FUN=c(mean,sd,length,sum))
+
+
 new_reviews$Like_Restaurant <- 0
 new_reviews[new_reviews$stars >= 4,]$Like_Restaurant <- 1
 
-new_reviews$U_Service <- NA
-new_reviews$U_Speed <- NA
-new_reviews$U_Cleanliness <- NA
-new_reviews$U_Environment <- NA
-new_reviews$U_Taste <- NA
-new_reviews$U_Location <- NA
-new_reviews$U_Eastern_Asian <- NA
-new_reviews$U_Japanese <- NA
-new_reviews$U_Mexican <- NA
-new_reviews$U_Indian <- NA
-new_reviews$U_Italian <- NA
-new_reviews$U_Southeast_Asian <- NA
-new_reviews$U_American <- NA
-new_reviews$U_Middle_Eastern <- NA
-new_reviews$U_Latin_American <- NA
-new_reviews$U_Mediterranean <- NA
-new_reviews$U_European <- NA
-new_reviews$U_Fastfood <- NA
-new_reviews$U_Pizza <- NA
-new_reviews$U_Seafood <- NA
-new_reviews$U_Breakfast <- NA
-new_reviews$U_Cafe <- NA
-new_reviews$U_Diners <- NA
-new_reviews$U_Bars <- NA
-new_reviews$B_Service <- NA
-new_reviews$B_Speed <- NA
-new_reviews$B_Cleanliness <- NA
-new_reviews$B_Taste <- NA
-new_reviews$B_Environment <- NA
-new_reviews$B_Location <- NA
-
-for (row in 1:nrow(new_reviews))
-{
-  review_id = new_reviews[row, "review_id"]
-  user_id   = new_reviews[row, "user_id"]
-  business_id = new_reviews[row, "business_id"]
-  
-  #perform summary of user_id
-  sub_new_reviews <- new_reviews[new_reviews$user_id == user_id & new_reviews$review_id != review_id,]
-  if (nrow(sub_new_reviews) > 0)
-  {
-
-    new_reviews[row,]$U_Service <- mean(sub_new_reviews$service)
-    new_reviews[row,]$U_Speed <- mean(sub_new_reviews$speed)
-    new_reviews[row,]$U_Cleanliness <- mean(sub_new_reviews$cleanliness)
-    new_reviews[row,]$U_Environment <- mean(sub_new_reviews$environment)
-    new_reviews[row,]$U_Taste <- mean(sub_new_reviews$taste)
-    new_reviews[row,]$U_Location <- mean(sub_new_reviews$location)
-    new_reviews[row,]$U_Eastern_Asian <- mean(sub_new_reviews$Eastern_Asian)
-    new_reviews[row,]$U_Japanese <- mean(sub_new_reviews$Japanese)
-    new_reviews[row,]$U_Mexican <- mean(sub_new_reviews$Mexican)
-    new_reviews[row,]$U_Indian <- mean(sub_new_reviews$Indian)
-    new_reviews[row,]$U_Southeast_Asian <- mean(sub_new_reviews$Southeast_Asian)
-    new_reviews[row,]$U_American <- mean(sub_new_reviews$American)
-    new_reviews[row,]$U_Italian <- mean(sub_new_reviews$Italian)
-    new_reviews[row,]$U_Middle_Eastern <- mean(sub_new_reviews$Middle_Eastern)
-    new_reviews[row,]$U_Latin_American <- mean(sub_new_reviews$Latin_American)
-    new_reviews[row,]$U_Mediterranean <- mean(sub_new_reviews$Mediterranean)
-    new_reviews[row,]$U_European <- mean(sub_new_reviews$European)
-    new_reviews[row,]$U_Fastfood <- mean(sub_new_reviews$Fastfood)
-    new_reviews[row,]$U_Seafood <- mean(sub_new_reviews$Seafood)
-    new_reviews[row,]$U_Pizza <- mean(sub_new_reviews$Pizza)
-    new_reviews[row,]$U_Breakfast <- mean(sub_new_reviews$Breakfast)
-    new_reviews[row,]$U_Cafe <- mean(sub_new_reviews$Cafe)
-    new_reviews[row,]$U_Diners <- mean(sub_new_reviews$Diners)
-    new_reviews[row,]$U_Bars <- mean(sub_new_reviews$Bars)
-  }
-
-  #perform summary of business id
-  sub_new_reviews <- new_reviews[new_reviews$business_id == business_id & new_reviews$review_id != review_id,]
-  if (nrow(sub_new_reviews) > 0)
-  {  
-
-    new_reviews[row,]$B_Service <- sum(sub_new_reviews$service_rating)/sum(sub_new_reviews$service)
-    new_reviews[row,]$B_Speed <- sum(sub_new_reviews$speed_rating)/sum(sub_new_reviews$speed)
-    new_reviews[row,]$B_Cleanliness <- sum(sub_new_reviews$cleanliness_rating)/sum(sub_new_reviews$cleanliness)
-    new_reviews[row,]$B_Taste <- sum(sub_new_reviews$taste_rating)/sum(sub_new_reviews$taste)
-    new_reviews[row,]$B_Environment <- sum(sub_new_reviews$environment_rating)/sum(sub_new_reviews$environment)
-    new_reviews[row,]$B_Location <- sum(sub_new_reviews$location_rating)/sum(sub_new_reviews$location)
-  }
-  print(row)
-}
+review_analysis_data <- merge(merge(new_reviews,
+                                    user_revs,by='user_id')
+                              ,bus_revs,by='business_id')
 
 
-summary(new_reviews)
+summary(review_analysis_data)
 
+review_analysis_data$U_service <- ((review_analysis_data$service.mean*review_analysis_data$service.length) - review_analysis_data$service) / (review_analysis_data$service.length-1)
+review_analysis_data$U_environment<- ((review_analysis_data$environment.mean*review_analysis_data$environment.length) - review_analysis_data$environment) / (review_analysis_data$environment.length-1)
+review_analysis_data$U_Mexican<- ((review_analysis_data$Mexican.mean*review_analysis_data$Mexican.length) - review_analysis_data$Mexican) / (review_analysis_data$Mexican.length-1)
+review_analysis_data$U_Middle_Eastern<- ((review_analysis_data$Middle_Eastern.mean*review_analysis_data$Middle_Eastern.length) - review_analysis_data$Middle_Eastern) / (review_analysis_data$Middle_Eastern.length-1)
+review_analysis_data$U_European<- ((review_analysis_data$European.mean*review_analysis_data$European.length) - review_analysis_data$European) / (review_analysis_data$European.length-1)
+review_analysis_data$U_Cafe<- ((review_analysis_data$Cafe.mean*review_analysis_data$Cafe.length) - review_analysis_data$Cafe) / (review_analysis_data$Cafe.length-1)
+review_analysis_data$U_speed<- ((review_analysis_data$speed.mean*review_analysis_data$speed.length) - review_analysis_data$speed) / (review_analysis_data$speed.length-1)
+review_analysis_data$U_location<- ((review_analysis_data$location.mean*review_analysis_data$location.length) - review_analysis_data$location) / (review_analysis_data$location.length-1)
+review_analysis_data$U_Indian<- ((review_analysis_data$Indian.mean*review_analysis_data$Indian.length) - review_analysis_data$Indian) / (review_analysis_data$Indian.length-1)
+review_analysis_data$U_Latin_American<- ((review_analysis_data$Latin_American.mean*review_analysis_data$Latin_American.length) - review_analysis_data$Latin_American) / (review_analysis_data$Latin_American.length-1)
+review_analysis_data$U_Fastfood<- ((review_analysis_data$Fastfood.mean*review_analysis_data$Fastfood.length) - review_analysis_data$Fastfood) / (review_analysis_data$Fastfood.length-1)
+review_analysis_data$U_Diners<- ((review_analysis_data$Diners.mean*review_analysis_data$Diners.length) - review_analysis_data$Diners) / (review_analysis_data$Diners.length-1)
+review_analysis_data$U_cleanliness<- ((review_analysis_data$cleanliness.mean*review_analysis_data$cleanliness.length) - review_analysis_data$cleanliness) / (review_analysis_data$cleanliness.length-1)
+review_analysis_data$U_Eastern_Asian<- ((review_analysis_data$Eastern_Asian.mean*review_analysis_data$Eastern_Asian.length) - review_analysis_data$Eastern_Asian) / (review_analysis_data$Eastern_Asian.length-1)
+review_analysis_data$U_Southeast_Asian<- ((review_analysis_data$Southeast_Asian.mean*review_analysis_data$Southeast_Asian.length) - review_analysis_data$Southeast_Asian) / (review_analysis_data$Southeast_Asian.length-1)
+review_analysis_data$U_Mediterranean<- ((review_analysis_data$Mediterranean.mean*review_analysis_data$Mediterranean.length) - review_analysis_data$Mediterranean) / (review_analysis_data$Mediterranean.length-1)
+review_analysis_data$U_Seafood<- ((review_analysis_data$Seafood.mean*review_analysis_data$Seafood.length) - review_analysis_data$Seafood) / (review_analysis_data$Seafood.length-1)
+review_analysis_data$U_Bars<- ((review_analysis_data$Bars.mean*review_analysis_data$Bars.length) - review_analysis_data$Bars) / (review_analysis_data$Bars.length-1)
+review_analysis_data$U_taste<- ((review_analysis_data$taste.mean*review_analysis_data$taste.length) - review_analysis_data$taste) / (review_analysis_data$taste.length-1)
+review_analysis_data$U_Japanese<- ((review_analysis_data$Japanese.mean*review_analysis_data$Japanese.length) - review_analysis_data$Japanese) / (review_analysis_data$Japanese.length-1)
+review_analysis_data$U_American<- ((review_analysis_data$American.mean*review_analysis_data$American.length) - review_analysis_data$American) / (review_analysis_data$American.length-1)
+review_analysis_data$U_Italian<- ((review_analysis_data$Italian.mean*review_analysis_data$Italian.length) - review_analysis_data$Italian) / (review_analysis_data$Italian.length-1)
+review_analysis_data$U_Pizza<- ((review_analysis_data$Pizza.mean*review_analysis_data$Pizza.length) - review_analysis_data$Pizza) / (review_analysis_data$Pizza.length-1)
+review_analysis_data$U_Breakfast<- ((review_analysis_data$Breakfast.mean*review_analysis_data$Breakfast.length) - review_analysis_data$Breakfast) / (review_analysis_data$Breakfast.length-1)
+
+review_analysis_data$B_Service <- (review_analysis_data$service_rating.sum - review_analysis_data$service_rating) /(review_analysis_data$service_rating.length-1)
+review_analysis_data$B_Speed <- (review_analysis_data$speed_rating.sum - review_analysis_data$speed_rating) /(review_analysis_data$speed_rating.length-1)
+review_analysis_data$B_Cleanliness <- (review_analysis_data$cleanliness_rating.sum - review_analysis_data$cleanliness_rating) /(review_analysis_data$cleanliness_rating.length-1)
+review_analysis_data$B_Taste <- (review_analysis_data$taste_rating.sum - review_analysis_data$taste_rating) /(review_analysis_data$taste_rating.length-1)
+review_analysis_data$B_Environment <- (review_analysis_data$environment_rating.sum - review_analysis_data$environment_rating) /(review_analysis_data$environment_rating.length-1)
+review_analysis_data$B_Location <- (review_analysis_data$location_rating.sum - review_analysis_data$location_rating) /(review_analysis_data$location_rating.length-1)
